2bb8fb5e4ee4909e634a592da26ee12aa13e2308
RFK <kakigori11235@gmail.com>
2025-09-28 22:35:36 -0400

    finished

--- Assets/Shaders/Includes/LightingHelp.hlsl
+++ Assets/Shaders/Includes/LightingHelp.hlsl
@@ -1,4 +1,4 @@
-void GetMainLight_float(float3 WorldPos, out float3 Color, out float3 Direction, out float DistanceAtten, out float ShadowAtten) {
+﻿void GetMainLight_float(float3 WorldPos, out float3 Color, out float3 Direction, out float DistanceAtten, out float ShadowAtten) {
 #ifdef SHADERGRAPH_PREVIEW
     Direction = normalize(float3(0.5, 0.5, 0));
     Color = 1;
@@ -31,3 +31,45 @@ void ChooseColor_float(float3 Highlight, float3 Shadow, float Diffuse, float Thr
         OUT = Highlight;
     }
 }
+
+void ChooseTriColor_float(float3 Highlight, float3 Shadow, float Diffuse, float3 Midtone, float Threshold, float Threshold2, out float3 OUT)
+{
+    if (Diffuse < Threshold)
+    {
+        OUT = Shadow;
+    }
+    else if (Diffuse < Threshold2)
+    {
+        OUT = Midtone;
+    }
+    else
+    {
+        OUT = Highlight;
+    }
+}
+
+void ChooseTriColorSmooth_float(float3 Highlight, float3 Shadow, float Diffuse, float3 Midtone, float Threshold, float Threshold2, float Smoothness, out float3 OUT)
+{
+    float smoothness = 0.1;
+
+    float t0 = smoothstep(Threshold - smoothness, Threshold + smoothness, Diffuse);
+    float3 shadowToMidtone = lerp(Shadow, Midtone, t0);
+
+    float t1 = smoothstep(Threshold2 - smoothness, Threshold2 + smoothness, Diffuse);
+
+    OUT = lerp(shadowToMidtone, Highlight, t1);
+}
+
+void addStripes_float(float shadowAtten, float3 Shadow, float3 Currtone, float stripes, out float3 OUT)
+{
+    if (shadowAtten < 0.3)
+    {
+        OUT = Shadow * (1 - stripes) + Currtone * stripes;
+    }
+    else
+    {
+        OUT = Currtone;
+    }
+
+}
+
